b89b3e67c0f6f641a9d469612e1d199f96034fc9
Lucas-M01 <lucas.montenegro.n@outlook.com>
2022-07-20 16:28:50 -0300

    Adicionando os modulos, organizando rotas e sistema de cadastro

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,4 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req,res)=>{
-    res.send("user test is successfull")
-})
-
-router.post("/userposttest", (req,res)=>{
-    const username = req.body.username
-    res.send(`Seu nome de usuário é: ${username}`)
-})
 
 module.exports = router
