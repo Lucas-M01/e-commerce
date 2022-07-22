4c3ef16d966768fff7e9d81e709f68a36bf2f5e2
Lucas-M01 <lucas.montenegro.n@outlook.com>
2022-07-21 21:16:17 -0300

    Deletar usuário

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const User = require("../models/User");
 
 const router = require("express").Router();
@@ -14,6 +14,26 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
             $set: req.body
         },{new:true});
         res.status(200).json(updatedUser);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Deletar
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("Usuário apagado")
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Pegar Usuário
+router.get("/:id", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        res.status(200).json("Usuário apagado")
     }catch(err){
         res.status(500).json(err)
     }
